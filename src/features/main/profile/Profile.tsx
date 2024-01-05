5c86921904878ade4e5cc0cd843adc82adac6fc1
k2 <izmail.k2@gmail.com>
2024-01-06 00:42:43 +0300

    feat: Updated Header component styles and Profile animation

--- src/features/main/profile/Profile.tsx
+++ src/features/main/profile/Profile.tsx
@@ -1,7 +1,32 @@
-export const Profile = () => {
-  return (
-    <div>
+import s from './Profile.module.scss'
+import circle from '@/assets/image/profile/circle.jpg'
+import square from '@/assets/image/profile/square.jpg'
+import triangle from '@/assets/image/profile/triangle.jpg'
+import {createRef, useLayoutEffect} from "react";
 
-    </div>
+
+export const Profile = () => {
+  const images = [{id: 0, name: circle}, {id: 1, name: square}, {id: 2, name: triangle}]
+  const imgRefs = images.map(() => createRef<HTMLImageElement>())
+  
+  useLayoutEffect(() => {
+    requestAnimationFrame(() => {
+      imgRefs.forEach((el) => el.current?.classList.add(s.imgActive))
+    })
+  }, [])
+
+
+  return (
+    <section className={s.container}>
+      <div className={`containerApp ${s.block}`}>
+        <div className={s.decoratorBc}>
+          {images.map((img, index) => <img key={img.id} src={img.name} alt="img" ref={imgRefs[index]}/>)}
+        </div>
+        <form className={s.form}>
+          <div>sss</div>
+        </form>
+      </div>
+    </section>
   );
 };
+
