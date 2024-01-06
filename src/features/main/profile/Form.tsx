f38590e4de6faf419395d043e1076e89d474753d
k2 <izmail.k2@gmail.com>
2024-01-06 22:52:52 +0300

    style: component Form full style

--- src/features/main/profile/Form.tsx
+++ src/features/main/profile/Form.tsx
@@ -1,13 +1,25 @@
 import s from './Form.module.scss'
+import {BtnPoly} from "@/common/components/btnPoly/BtnPoly.tsx";
 
 
 export const Form = () => {
 
 
   return (
-        <form className={s.form}>
-          <div>sss</div>
-        </form>
+    <div className={s.container}>
+      <h2 className={'h2'}>Provide reliable news</h2>
+      <form className={s.form}>
+        <label className={s.input}>
+          <span>Post title</span>
+          <input type="text"/>
+        </label>
+        <label className={s.textarea}>
+          <span>Post Description</span>
+          <textarea/>
+        </label>
+        <BtnPoly variant={'primary'}>Add Post</BtnPoly>
+      </form>
+    </div>
   );
 };
 
--- src/features/main/profile/Profile.tsx
+++ src/features/main/profile/Profile.tsx
@@ -4,10 +4,12 @@ import square from '@/assets/image/profile/square.jpg'
 import triangle from '@/assets/image/profile/triangle.jpg'
 import {createRef, useLayoutEffect} from "react";
 import {Form} from "@/features/main/profile/Form.tsx";
+import {IconSvg} from "@/common/components/iconSvg/IconSvg.tsx";
 
 
 export const Profile = () => {
   const images = [{id: 0, name: circle}, {id: 1, name: square}, {id: 2, name: triangle}]
+  const svg = ['dots', 'wave'] as const
   const imgRefs = images.map(() => createRef<HTMLImageElement>())
 
   useLayoutEffect(() => {
@@ -22,6 +24,7 @@ export const Profile = () => {
       <div className={`containerApp ${s.block}`}>
         <div className={s.decoratorBc}>
           {images.map((img, index) => <img key={img.id} src={img.name} alt="img" ref={imgRefs[index]}/>)}
+          {svg.map((svg) => <div className={s.svg}><IconSvg name={svg}/></div>)}
         </div>
         <Form/>
       </div>
