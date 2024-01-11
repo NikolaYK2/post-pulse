82c9bdd667efa94d08cb908c39d7de38da5ffe7c
k2 <izmail.k2@gmail.com>
2024-01-11 21:49:30 +0300

    refactor: update component Form, delete two useState title and discription and change post useState

--- src/features/main/profile/Form.tsx
+++ src/features/main/profile/Form.tsx
@@ -4,54 +4,46 @@ import {H2} from "@/common/components/ui/h2/H2.tsx";
 import {ChangeEvent, Dispatch, SetStateAction, useState} from "react";
 
 
-
 type CommonFields = {
   id: string;
   title: string;
   description: string;
-  data:string
+  data: string
 };
 
-type Props<T extends  CommonFields> = {
+type Props<T extends CommonFields> = {
   posts: T[],
   setPosts: Dispatch<SetStateAction<T[]>>;
 }
-export const Form = <T  extends  CommonFields>({posts, setPosts}: Props<T>) => {
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
+export const Form = <T extends CommonFields>({posts, setPosts}: Props<T>) => {
+  const [post, setPost] = useState({title: '', description: ''})
   const uniqueId = `${Date.now()}-${Math.random()}`;
 
 
   const addTitleHandle = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.currentTarget.value)
+    setPost({...post, title: e.currentTarget.value})
   }
   const addDescriptionHandle = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setDescription(e.currentTarget.value)
+    setPost({...post, description: e.currentTarget.value})
   }
 
   const addPostHandle = () => {
-    const newPost = {
-      id: uniqueId,
-      title: title,
-      description: description,
-      data:''
-    } as T;
-
-    setPosts([...posts, newPost])
-    setTitle('')
-    setDescription('')
+    setPosts([...posts, {...post, id: uniqueId, data: Date + ''} as T])
+    setPost({title: '', description: ''})
   }
+
+
   return (
     <div className={s.container} onSubmit={event => event.preventDefault()}>
       <H2 title={'Provide reliable news'} side={'left'}/>
       <form className={s.form}>
         <label className={s.input}>
           <span>Post title</span>
-          <input type="text" value={title} onChange={addTitleHandle}/>
+          <input type="text" value={post.title} onChange={addTitleHandle}/>
         </label>
         <label className={s.textarea}>
           <span>Post Description</span>
-          <textarea value={description} onChange={addDescriptionHandle}/>
+          <textarea value={post.description} onChange={addDescriptionHandle}/>
         </label>
         <BtnPoly variant={'primary'} onClick={addPostHandle}>Add Post</BtnPoly>
       </form>
