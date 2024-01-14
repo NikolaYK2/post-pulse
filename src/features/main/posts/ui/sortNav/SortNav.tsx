26b64a59362d0fc64e3fa1ec7664c68b1cbbc50a
k2 <izmail.k2@gmail.com>
2024-01-14 16:57:51 +0300

    refactor(feat): add obj sortFn & useState sortArr, refactor sortHandle

--- src/features/main/posts/ui/sortNav/SortNav.tsx
+++ src/features/main/posts/ui/sortNav/SortNav.tsx
@@ -4,40 +4,44 @@ import {PostsType} from "@/app/postRpovider/PostProvider.tsx";
 import {usePosts} from "@/app/postRpovider/usePosts.tsx";
 import {useState} from "react";
 
+type SortFunction = {
+  [key: string]: () => void;
+};
 type Props = {
   allPosts: PostsType[]
   setAllPosts: (posts: PostsType[]) => void
 }
 export const SortNav = ({allPosts, setAllPosts}: Props) => {
   const [isSorted, setIsSorted] = useState(false)
+
   const {posts} = usePosts()
 
-  const sortArr = ['default', 'data', 'title',]
+  const [sortArr, setSortArr] = useState([
+    {name: 'default', isActive: true},
+    {name: 'data', isActive: false},
+    {name: 'title', isActive: false},])
 
+  const sortFunctions: SortFunction = {
+    'data': () => setAllPosts([...allPosts].sort((a, b) => isSorted ? a.data.localeCompare(b.data) : b.data.localeCompare(a.data))),
+    'title': () => setAllPosts([...allPosts].sort((a, b) => a.title.localeCompare(b.title))),
+    'default': () => setAllPosts(posts),
+  }
 
-  const sortHandle = (el: string) => {
-    if (el === 'data') {
-      if (isSorted) {
-        setAllPosts([...allPosts].sort((a, b) => a.data.localeCompare(b.data)))
-      } else {
-        setAllPosts([...allPosts].sort((a, b) => b.data.localeCompare(a.data)))
-      }
-    }
-
-    if (el === 'title') setAllPosts([...allPosts].sort((a, b) => a.title.localeCompare(b.title)))
-    if (el === 'default') setAllPosts(posts)
-    setIsSorted(!isSorted);
-
+  const sortHandle = (value: string) => {
+    sortFunctions[value]()
+    setSortArr(sortArr.map(el => el.name === value ? {...el, isActive: true} : {...el, isActive: false}))
+    setIsSorted(!isSorted)
   }
 
   return (
     <nav className={s.container}>
       <ul className={s.blockItem}>
-        {sortArr.map(el => <li key={el}>
-          <BtnPoly className={s.btn} onClick={() => sortHandle(el)}>
-            {el}
+        {sortArr.map(el =>
+          <BtnPoly key={el.name} className={`${s.btn} ${el.isActive ? s.isActive : ''}`}
+                   onClick={() => sortHandle(el.name)}>
+            {el.name}
           </BtnPoly>
-        </li>)}
+        )}
       </ul>
     </nav>
   );
